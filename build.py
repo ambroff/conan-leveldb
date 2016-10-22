6a2cf915a31c66a6e55bd708c0aaccab70e186e7
Lasote <lasote@gmail.com>
2016-10-22 17:59:01 +0200

    libstdc++11 default for gcc 5

--- build.py
+++ build.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
         for settings, options in builder.builds:
             if settings["arch"] != "x86":
                 filtered_builds.append([settings, options])
+            if settings["compiler"] == "gcc" and float(settings["compiler.version"]) > 5:
+                settings["compiler.libcxx"] = "libstdc++11"
         builder.builds = filtered_builds
     builder.run()
 
